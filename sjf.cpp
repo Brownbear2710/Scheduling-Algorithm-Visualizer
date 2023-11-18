11802ae0882fb3b6232b7d67388ef2686a2c7eb7
Adib Farhan <adibfarhan@iut-dhaka.edu>
2023-11-19 00:08:29 +0600

    Update sjf.cpp
    Did what needed to be done

---
+++ sjf.cpp
@@ -0,0 +1,20 @@
+#include "srtf.cpp"
+
+namespace sjf
+{
+    bool show_wait_time = true;
+    bool visual_output = true;
+    vector<int> simulate(vector<ProcessState> &states)
+    {
+        for(int i = 0; i < states.size(); i++)
+            states[i].AT = 0;
+        bool tmp_visual_output = srtf::visual_output;
+        bool tmp_show_wait_time = srtf::show_wait_time;
+        srtf::visual_output = visual_output;
+        srtf::show_wait_time = show_wait_time;
+        auto ret = srtf::simulate(states, "- SJF (Shortest Job First) [All processes arrive at 0]:");
+        srtf::visual_output = tmp_visual_output;
+        srtf::show_wait_time = tmp_show_wait_time;
+        return ret;
+    }
+}
